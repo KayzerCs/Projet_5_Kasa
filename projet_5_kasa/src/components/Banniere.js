62b2ae110c26206acd44c269886b8f4fb8ccaaa9
Kayzer <max.mlv2210@gmail.com>
2024-06-10 11:29:37 +0200

    Bannière

---
+++ projet_5_kasa/src/components/Banniere.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/components/_Banniere.scss";
+
+
+const Banniere = () => {
+  return (
+    <div className="body">
+      <div className="banniere"></div>
+    </div>
+  );
+};
+
+export default Banniere;
--- projet_5_kasa/src/pages/About.js
+++ projet_5_kasa/src/pages/About.js
@@ -1,11 +1,15 @@
 import React from "react";
 import NavLogo from "../components/NavLogo";
 import Footer from "../components/Footer";
+import Banniere from "../components/Banniere";
 
 const About = () => {
   return (
     <div>
       <NavLogo />
+      <div>
+        <Banniere />
+      </div>
       <Footer />
     </div>
   );
