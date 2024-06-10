838d7c23ddc0245ffb0060c0a820e78cfafd3119
Kayzer <max.mlv2210@gmail.com>
2024-06-10 10:43:28 +0200

    page 404

--- projet_5_kasa/src/pages/NotFound.js
+++ projet_5_kasa/src/pages/NotFound.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NavLogo from "../components/NavLogo";
 import Footer from "../components/Footer";
 
@@ -6,6 +7,11 @@ const NotFound = () => {
   return (
     <div>
       <NavLogo />
+      <div className="container_p">
+        <p className="number">404</p>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link className="link" to="/">Retourner sur la page d'acceuil</Link>
+      </div>
       <Footer />
     </div>
   );
